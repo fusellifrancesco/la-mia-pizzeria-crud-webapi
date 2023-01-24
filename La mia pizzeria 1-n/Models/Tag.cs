eb09977ca7ddab264634c15cb78d94432cbcfd73
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-24 22:33:43 +0100

    Aggiunto [JsonIgnore] e ? su Model Tag

--- La mia pizzeria 1-n/Models/Tag.cs
+++ La mia pizzeria 1-n/Models/Tag.cs
@@ -1,10 +1,13 @@
-﻿namespace La_mia_pizzeria_1_n.Models {
+﻿using System.Text.Json.Serialization;
+
+namespace La_mia_pizzeria_1_n.Models {
     public class Tag {
 
         public int Id { get; set; }
         public string Title { get; set; }
-        
-        public List<Pizza> Pizze { get; set; }
+
+        [JsonIgnore]
+        public List<Pizza>? Pizze { get; set; }
 
         public Tag() {
 
