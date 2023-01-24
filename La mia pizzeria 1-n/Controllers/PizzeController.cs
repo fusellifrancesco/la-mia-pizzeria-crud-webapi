75773f6b8363bf33c92efbdaf08e88694f595a45
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-24 22:24:26 +0100

    Creato e completato PizzeController per implementazione webapi

---
+++ La mia pizzeria 1-n/Controllers/PizzeController.cs
@@ -0,0 +1,23 @@
+﻿using La_mia_pizzeria_1_n.Database;
+using La_mia_pizzeria_1_n.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace La_mia_pizzeria_1_n.Controllers {
+    
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzeController : ControllerBase {
+        [HttpGet]
+        public IActionResult Get() {
+            using (PizzaContext db = new PizzaContext()) {
+                List<Pizza> Pizze = db.Pizze.Include(articolo => articolo.Tags).ToList<Pizza>();
+
+                return Ok(Pizze);
+            }
+        }
+
+
+    }
+}
